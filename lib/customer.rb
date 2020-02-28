a5ec91788afaea40701583344fd2a1f147799314
Steven Riggs <sfriggs21@gmail.com>
2020-02-27 21:48:14 -0700

    final solution

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,34 @@
 class Customer
-  
+  attr_reader :name, :age
+
+
+  # class variables
+  @@all = []
+
+  # class methods
+  def self.all
+    @@all
+  end
+
+
+  # instance methods
+  def initialize(name, age)
+    @@all << self
+
+    @name = name
+    @age = age
+  end
+
+  def new_meal(waiter, total, tip=0)
+    Meal.new(waiter, self, total, tip)
+  end
+
+  def meals
+    Meal.all.select {|meal| meal.customer == self}
+  end
+
+  def waiters
+    meals.collect {|meal| meal.waiter}
+  end
+
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,34 @@
 class Waiter
+  attr_reader :name, :yrs_experience
+
+
+  # class variables
+  @@all = []
+
+  # class methods
+  def self.all
+    @@all
+  end
+
+
+  # instance methods
+  def initialize(name, yrs_experience)
+    @@all << self
+
+    @name = name
+    @yrs_experience = yrs_experience
+  end
+
+  def new_meal(customer, total, tip=0)
+    Meal.new(self, customer, total, tip)
+  end
+
+  def meals
+    Meal.all.select {|meal| meal.waiter == self}
+  end
+
+  def best_tipper
+    meals.sort {|a, b| a.tip <=> b.tip}.last.customer
+  end
 
 end
