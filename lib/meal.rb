04330ae68b303e14c0ef38a98d2eb6db234885ac
Steven Riggs <sfriggs21@gmail.com>
2020-02-28 14:40:53 +0000

    Done.

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,24 +1,24 @@
 class Meal
   attr_reader :waiter, :customer, :total, :tip
-
-
+  
+  
   # class variables
   @@all = []
-
+  
   # class methods
   def self.all
     @@all
   end
-
-
+  
+  
   # instance methods
   def initialize(waiter, customer, total, tip=0)
     @@all << self
-
+    
     @waiter = waiter
     @customer = customer
     @total = total
     @tip = tip
   end
-
+  
 end
