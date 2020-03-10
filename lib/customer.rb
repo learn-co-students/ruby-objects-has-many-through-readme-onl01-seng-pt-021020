d8f4f535c92578f73b2341d9a8c2d9d959ed700b
John J Marshall Jr <johnjamesmarshalljr@gmail.com>
2020-03-10 21:20:56 +0000

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -12,4 +12,20 @@ class Customer
     @@all
   end
 
+  def new_meal(waiter, total, tip=0)
+    Meal.new(waiter, self, total, tip)
+  end
+
+  def waiters
+   meals.map do |meal|
+     meal.waiter
+   end
+ end
+
+
+  def meals
+    Meal.all.select do |meal| meal.customer == self
+    end
+  end
+
 end
