114cbe0b81f91d7a239ec2ab6d2dd8f99dd78b74
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-03-10 22:05:29 +0000

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,46 @@
+require 'pry'
 class Customer
+  attr_accessor :name, :age 
+  
+  @@all = [] #all customers 
+  
+  def initialize(name, age)
+    @name = name
+    @age = age 
+    @@all << self #self is a customer being added to @@all array
+  end 
+  
+  def self.all
+    @@all
+  end 
+  
+  def new_meal(waiter, total, tip = 0)
+    Meal.new(waiter, self, total, tip)
+    #customer not taken in as an argument because self is customer. 
+    #This method allows us to create new meals as a customer
+    #Assosciate each new meal with the customer that created it. 
+  end 
+  
+  def meals
+    Meal.all.select do |meal|
+      meal.customer == self 
+    end 
+    #iterating through every instance of Meal and returning only the ones whose meal's customer matches the current customer instance. 
+  end 
+  
+  def waiters
+    meals.map do |meal|
+      meal.waiter 
+    end 
+    #we now want a list of all the waiters that our customer has interacted with. 
+    #each meal is associated with a waiter, so to get every waiter
+    #we take the array of all customer's meals 
+    #map over it to get a NEW array, getting the waiter from each of those meals 
+    
+  end 
+  
+  
+  
+  
   
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,36 @@
 class Waiter
-
+  attr_accessor :name, :yrs_experience 
+  @@all = [] #all waiters
+  
+  def initialize(name, yrs_experience)
+    @name = name 
+    @yrs_experience = yrs_experience
+    @@all << self 
+  end 
+  
+  def self.all
+    @@all
+  end 
+  
+  def new_meal(customer, total, tip = 0)
+    Meal.new(self, customer, total, tip)
+    #Parameters are different for the new_meal method between customer and waiter, but order of argeuments for Meal.new remains the same. 
+  end 
+  
+  def meals 
+    Meal.all.select do |meal|
+      meal.waiter == self 
+    end 
+  end 
+  #This is the other direction...this is how a waiter can find a customer 
+  
+  def best_tipper
+    best_tipped_meal = meals.max do |meal_a, meal_b|
+      meal_a.tip <=> meal_b.tip
+    end 
+    best_tipped_meal.customer
+  end 
+  
+  
+  
 end
