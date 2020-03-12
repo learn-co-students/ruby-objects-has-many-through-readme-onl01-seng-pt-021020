24a294868cb01c6cfbf46abf378d073374e5b05f
Amber Williams <awilliams1234@live.com>
2020-03-12 03:29:46 +0000

    Done.

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -12,5 +12,16 @@ class Waiter
   def self.all
     @@all 
   end 
+  
+  def new_meal(customer, total, tip=0)
+    Meal.new(self, customer, total, tip)
+  end 
+  
+  def meals
+    Meal.all.select do |meal| 
+      meal.waiter == self 
+    end 
+  end 
+    
 
 end
